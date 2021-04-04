39ac0b6a8266cb5df86c331485b4e12b76bfdb99
kim dongjin <1222kdj@gmail.com>
2021-04-04 22:40:26 +0900

    chap3 その２

--- src/chap3/list.scala
+++ src/chap3/list.scala
@@ -6,3 +6,27 @@ val oneTwoThreeFour = oneTwo ::: threeFour
 
 println(oneTwo + "and " + threeFour + "were not mutated")
 println("thus, " + oneTwoThreeFour + "is a new list")
+
+val nilList = Nil
+val nilList2 = List()
+val thrill = "Will" :: "fill" :: "until" :: Nil
+println(thrill(2))
+println(thrill.count(s => s.length == 4))
+println(thrill.drop(2))
+println(thrill.dropRight(2))
+println(thrill.exists(s => s.length == 4))
+println(thrill.filter(s => s.length == 4))
+println(thrill.forall(s => s.endsWith("l")))
+thrill.foreach(s => println(s))
+thrill.foreach(println)
+println(thrill.head)
+println(thrill.init)
+println(thrill.isEmpty)
+println(thrill.last)
+println(thrill.length)
+println(thrill.map(s => s + "y"))
+println(thrill.mkString(", "))
+println(thrill.filterNot(s => s.length == 4))
+println(thrill.reverse)
+println(thrill.sortWith((s,t) => s.charAt(0).toLower < t.charAt(0).toLower))
+println(thrill.tail)
