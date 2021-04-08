e74df5bee55abe00402f7d345cd53d169885c232
kim dongjin <1222kdj@gmail.com>
2021-04-09 00:02:08 +0900

    chap4-2

---
+++ src/chap4/ChecksumAccumulator.scala
@@ -0,0 +1,24 @@
+import scala.collection.mutable
+
+class ChecksumAccumulator {
+  private var sum = 0;
+  def add(b: Byte): Unit = { sum += b }
+  def checkSum(): Int = ~(sum & 0xFF) + 1
+}
+
+object ChecksumAccumulator {
+  private val cache = mutable.Map.empty[String, Int]
+  def calculate(s: String): Int =
+    if (cache.contains(s))
+      cache(s)
+    else {
+      val acc = new ChecksumAccumulator
+      for (c <- s) 
+        acc.add(c.toByte)
+      val cs = acc.checkSum()
+      cache += (s -> cs)
+      cs
+    }
+}
+
+println(ChecksumAccumulator.calculate("Every value is an object"))
