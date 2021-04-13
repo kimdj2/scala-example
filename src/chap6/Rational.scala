08102e9fa63f950de38190f3e14f0bcd0a159a67
kim dongjin <1222kdj@gmail.com>
2021-04-13 23:18:53 +0900

    chap6-2

--- src/chap6/Rational.scala
+++ src/chap6/Rational.scala
@@ -1,11 +1,33 @@
 class Rational(n: Int, d: Int) {
   require(d != 0)
-  val numer: Int = n
-  val denom: Int = d
-  override def toString = n + "/" + d
-  def add(that: Rational): Rational =
+  private val g = gcd(n.abs, d.abs)
+  val numer: Int = n / g
+  val denom: Int = d / g
+  def this(n: Int) = this(n, 1)
+  override def toString = numer + "/" + denom
+  def + (that: Rational): Rational =
     new Rational(
       numer * that.denom + that.numer * denom, 
       denom * that.denom
     )
+  def * (that: Rational): Rational =
+    new Rational(numer * that.numer, denom * that.denom)
+  private def gcd(a: Int, b: Int): Int = 
+    if (b == 0) a else gcd(b, a % b)
 }
+
+val y = new Rational(3)
+println(y)
+
+val oneHalf = new Rational(1,2)
+val twoThirds = new Rational(2,3)
+
+val addResult = oneHalf + twoThirds
+println(addResult)
+
+val multiResult = oneHalf * twoThirds
+println(multiResult)
+
+val gcdResult = new Rational(77, 49)
+
+println(gcdResult)
