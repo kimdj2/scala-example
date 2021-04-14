d8fb097160f81e231a792f83b558af7fa2fbd488
kim dongjin <1222kdj@gmail.com>
2021-04-15 01:10:19 +0900

    chap7

---
+++ src/chap7/for.scala
@@ -0,0 +1,26 @@
+for (i <- 1 to 4)
+  println("Iteration" + i)
+for (i <- 1 until 4)
+  println("Iteration" + i)
+
+val filesHere = (new java.io.File(".")).listFiles
+println(filesHere)
+for (file <- filesHere if file.getName.endsWith(".scala"))
+  println(file)
+
+for (
+  file <- filesHere
+  if file.isFile
+  if file.getName.endsWith(".scala")
+) println(file)
+
+def fileLines(file: java.io.File) = 
+  scala.io.Source.fromFile(file).getLines().toList
+def grep(pattern: String) = 
+  for {
+    file <- filesHere
+    if file.getName.endsWith(".scala")
+    line <- fileLines(file)
+    if line.trim.matches(pattern)
+  } println(file + ": " + line.trim)
+grep(".*gcd.*")
