da7039334bd7f4e5c5100d65437b2beb2c9bb87e
kim dongjin <1222kdj@gmail.com>
2021-04-16 00:07:50 +0900

    chap

---
+++ src/chap7/match.scala
@@ -0,0 +1,9 @@
+val firstArg = if(!args.isEmpty) args(0) else ""
+val friend = 
+  firstArg match {
+    case "salt" => "pepper"
+    case "chips" => "salsa"
+    case "eggs" => "bacon"
+    case _ => "huh?"
+  }
+println(friend)
--- src/chap7/trycatch.scala
+++ src/chap7/trycatch.scala
@@ -1,5 +1,6 @@
 import java.io.FileReader
 import java.io.FileNotFoundException
+import java.io.IOException
 
 try {
   val f = new FileReader("input.txt")
