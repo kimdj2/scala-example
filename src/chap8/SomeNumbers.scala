a8fc61ebd42e257155d8b1089e5f4a483517e99b
kim dongjin <1222kdj@gmail.com>
2021-04-19 00:31:04 +0900

    chap8

--- src/chap8/SomeNumbers.scala
+++ src/chap8/SomeNumbers.scala
@@ -10,3 +10,16 @@ val f = (_: Int) + (_: Int)
 println(f(10, 5))
 
 someNumbers.foreach(println _)
+
+someNumbers.foreach(println)
+
+def sum(a: Int, b: Int, c: Int): Int = a + b + c
+println(sum(1,2,3))
+
+val a = sum _
+println(a(1,2,3))
+
+println(a.apply(1,2,3))
+
+val b = sum(1, _: Int, 3)
+println(b(2))
